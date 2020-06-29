ce9874bba44913993e9c5aeb170621d7f3a6e1fe
zhangjun <jun.zhang1@ly.com>
2020-06-29 21:51:36 +0800

    自定义ScalarFunction

---
+++ flink/src/main/java/sql/function/CustomScalarFunction.java
@@ -0,0 +1,54 @@
+package sql.function;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.functions.ScalarFunction;
+import org.apache.flink.types.Row;
+
+import java.util.stream.Stream;
+
+/**
+ * @author zhangjun 欢迎关注我的公众号[大数据技术与应用实战],获取更多精彩实战内容
+ *
+ * 自定义ScalarFunction
+ */
+public class CustomScalarFunction{
+	public static void main(String[] args) throws Exception{
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setParallelism(1);
+		StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+		//通过程序的方式来注册函数
+		SumFunction sumFunction = new SumFunction();
+		tableEnv.registerFunction("mysum", sumFunction);
+		Table table1 = tableEnv.sqlQuery("select mysum(1,2)");
+		tableEnv.toAppendStream(table1, Row.class).print();
+
+		//------------------------------------------------------
+
+		//通过sql的方式来注册函数
+		String className = SumFunction.class.getName();
+		String sql = "create temporary function default_catalog.default_database.mysum1" +
+		             " as '" + className + "'";
+		tableEnv.sqlUpdate(sql);
+		Table table2 = tableEnv.sqlQuery("select mysum1(3,4)");
+		tableEnv.toAppendStream(table2, Row.class).print();
+
+		//------------------------------------------------------
+
+		//列出来所有的函数，看是否包含我们定义的函数
+		String[] functions = tableEnv.listFunctions();
+		Stream.of(functions).filter(f->f.startsWith("mysum")).forEach(System.out::println);
+
+		env.execute("CustomScalarFunction");
+	}
+
+	public static class SumFunction extends ScalarFunction{
+
+		public Integer eval(Integer a, Integer b){
+			return a + b;
+		}
+
+	}
+}
